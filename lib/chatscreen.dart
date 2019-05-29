9414df1b73eeb99115f695de747a050197605bb9
htyap <hocktoh.yap@photobookworldwide.com>
2019-05-29 20:30:31 +0800

    add chat message class

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ht_chat_app/chatmessage.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,6 +7,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
@@ -34,13 +36,37 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+
+    ChatMessage message = new ChatMessage(                         //new
+      text: text,                                                  //new
+    );                                                             //new
+    setState(() {                                                  //new
+      _messages.insert(0, message);                                //new
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Happy chat")),
-      body: _buildTextComposer(),
+      body: new Column(                                        //modified
+        children: <Widget>[                                         //new
+          new Flexible(                                             //new
+            child: new ListView.builder(                            //new
+              padding: new EdgeInsets.all(8.0),                     //new
+              reverse: true,                                        //new
+              itemBuilder: (_, int index) => _messages[index],      //new
+              itemCount: _messages.length,                          //new
+            ),                                                      //new
+          ),                                                        //new
+          new Divider(height: 1.0),                                 //new
+          new Container(                                            //new
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),                  //new
+            child: _buildTextComposer(),                       //modified
+          ),                                                        //new
+        ],                                                          //new
+      ),
     );
   }
 }
