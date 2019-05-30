83434be9284797aa1510d0c9870bb9254d8fa514
htyap <hocktoh.yap@photobookworldwide.com>
2019-05-30 08:53:35 +0800

    add firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:ht_chat_app/chatscreen.dart';
+//import 'package:flutter/material.dart';
+////import 'package:ht_chat_app/chatscreen.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+////      home: ChatScreen(),
+//    );
+//  }
+//}
+//
+//
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:ht_chat_app/services/authentication.dart';
+import 'package:ht_chat_app/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: ChatScreen(),
-    );
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
